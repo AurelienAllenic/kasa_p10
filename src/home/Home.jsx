b4486ac223c44418536f8999990c6fd12d4c942d
AurelienAllenic <aurelien.allenic@gmail.com>
2023-07-12 14:40:20 +0200

    (feat) added homeHeader component and styled it responsively

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './home.css'
 import Nav from '../nav/Nav'
+import HomeHeader from './HomeHeader'
 const Home = () => {
   return (
     <div>
       <Nav />
+      <HomeHeader/>
     </div>
   )
 }
---
+++ src/home/HomeHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import imageHome from '../assets/src_home.jpg';
+
+const HomeHeader = () => {
+  return (
+    <article className='container_header_home'>
+      <div className='image-container'>
+        <img src={imageHome} alt="Chez vous, partout et ailleurs" className='img_header_home' />
+      </div>
+      <p className='content_home_header'><span className='first_part_content_header_home'>Chez vous,</span> <span className='second_part_content_header_home'>partout et ailleurs</span></p>
+    </article>
+  );
+};
+
+export default HomeHeader;
